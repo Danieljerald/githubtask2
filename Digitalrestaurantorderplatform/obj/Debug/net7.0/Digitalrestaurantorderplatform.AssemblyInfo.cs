d8301ec311d9f81d7f08c539cb26ffb4f6b74ca5
danielsamson <danielsamson19@karunya.edu.in>
2023-08-03 21:09:52 +0530

    changes in feedback

--- Digitalrestaurantorderplatform/obj/Debug/net7.0/Digitalrestaurantorderplatform.AssemblyInfo.cs
+++ Digitalrestaurantorderplatform/obj/Debug/net7.0/Digitalrestaurantorderplatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Digitalrestaurantorderplatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5893b0c9482a0d961a0364030bf38ccb3b66e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9395c96f683ce06f6c34ff58e4444daef7294518")]
 [assembly: System.Reflection.AssemblyProductAttribute("Digitalrestaurantorderplatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Digitalrestaurantorderplatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Feedback/obj/Debug/net7.0/Feedback.AssemblyInfo.cs
+++ Feedback/obj/Debug/net7.0/Feedback.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Feedback")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5893b0c9482a0d961a0364030bf38ccb3b66e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9395c96f683ce06f6c34ff58e4444daef7294518")]
 [assembly: System.Reflection.AssemblyProductAttribute("Feedback")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Feedback")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
